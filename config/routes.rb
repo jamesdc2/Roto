0ba6da4fcefa277881863dcdaa1d78e869ff944b
James Cline <jrcline09@gmail.com>
2012-03-04 00:03:24 +0000

    Made standings index the home page

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Roto::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    root :to => 'home#index'
+    root :to => 'standings#index'
 
   # See how all your routes lay out with "rake routes"
 
